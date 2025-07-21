7ebff7314929030c07ec9e86cdb087c19e4ebf51
inaam <inaam@DESKTOP-VPEJCHG>
2025-07-21 16:48:44 +0530

    database corrections

--- python/app.py
+++ python/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 from conn import get_db
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__ , template_folder="../templates", static_folder="../static")
+app.debug = True
+app.config['SECRET_KEY'] = 'your-secret-key'
+toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def index():
     hot_deals = []
     db = get_db()
-    sql = "SELECT * FROM hot_deals"
+    sql = "SELECT * FROM MenuItems"
     cursor = db.cursor()
     cursor.execute(sql)
     hot_deals = cursor.fetchall()
+    print(hot_deals)
     return render_template('index.html', hot_deals=hot_deals)
 
 
